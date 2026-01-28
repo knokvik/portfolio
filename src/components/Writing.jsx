77b8dd198997dbe3017b7697b962c9e13de03a82
Niraj Naphade <97009673+knokvik@users.noreply.github.com>
2026-01-28 17:38:03 +0530

    projects layout changed

--- src/components/Writing.jsx
+++ src/components/Writing.jsx
@@ -1,3 +1,5 @@
+import '../App.css'
+
 function Writing() {
   return (
     <main className="main-content">
@@ -6,28 +8,40 @@ function Writing() {
           <h1>Projects</h1>
           <div className="projects-list">
             <div className="project-item">
-              <h2><a href="https://github.com/knokvik/Inveto" target="_blank" rel="noopener noreferrer">Inveto</a></h2>
-              <p>Inveto is a versatile application designed to streamline store management and inventory control. With Inveto, users can effortlessly make entries, manage product details, and preview store layouts.</p>
+              <h2><a href="https://github.com/knokvik/Inveto" target="_blank" rel="noopener noreferrer"> Inveto</a></h2>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/Inveto" className=""/>
+              </div>
             </div>
             <div className="project-item">
               <h2><a href="https://github.com/knokvik/TypeRush" target="_blank" rel="noopener noreferrer">TypeRush</a></h2>
-              <p>Typerush is an exhilarating typing battle website crafted with cutting-edge technologies such as Socket.io for real-time interactions, Next.js for seamless rendering, and TypeScript for robust development.</p>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/TypeRush" className=""/>
+            </div>
             </div>
             <div className="project-item">
               <h2><a href="https://github.com/knokvik/PetTrove" target="_blank" rel="noopener noreferrer">PetTrove</a></h2>
-              <p>Pettrove is a Flutter app for pet owners to manage pet information, health records, and shopping needs in one place. Features include pet profiles with details like name, breed, and age, medication and health tracking.</p>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/PetTrove" className=""/>
+            </div>
             </div>
             <div className="project-item">
               <h2><a href="https://github.com/knokvik/dragonball" target="_blank" rel="noopener noreferrer">Dragonball</a></h2>
-              <p>This Flutter app is built with the Dragon Ball API to practice UI design. The goal was to implement proper classic color combinations for matching the app UI and making it responsive.</p>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/dragonball" className=""/>
+            </div>
             </div>
             <div className="project-item">
               <h2><a href="https://github.com/knokvik/echo" target="_blank" rel="noopener noreferrer">Echo</a></h2>
-              <p>Echo is a real-time AI chat with Convex Agents. It includes voice support with VAPI, document uploads to build a knowledge base with embeddings and RAG, and teaches the AI how to escalate to a human or auto-resolve issues.</p>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/echo" className=""/>
+            </div>
             </div>
             <div className="project-item">
               <h2><a href="https://github.com/knokvik/monster" target="_blank" rel="noopener noreferrer">Monster</a></h2>
-              <p>A frontend UI project inspired by the Monster Energy Brand, built using HTML, CSS, JS, and GSAP for animations.</p>
+              <div className='project-card'>
+                <img src="https://opengraph.githubassets.com/1/knokvik/monster" className=""/>
+            </div>
             </div>
           </div>
         </article>
